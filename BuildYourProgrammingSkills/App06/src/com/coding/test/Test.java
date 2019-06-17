17e6cb80a65e428787d5356e483fb6bee10e836e
ankitkumargcet <ankitkumargcet@gmail.com>
2019-06-17 23:09:39 +0530

    First Commit

---
+++ BuildYourProgrammingSkills/App06/src/com/coding/test/Test.java
@@ -0,0 +1,27 @@
+package com.coding.test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Please provide count of natural number: ");
+		System.out.print("Number: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		naturalNumber(n);
+		sc.close();
+
+	}
+
+	// Logic for finding remainder without '%' operator
+	public static void naturalNumber(int n) {
+		int number = 0;
+		System.out.println("Requested Natural numbers are:");
+		while(n>0) {
+			System.out.print(++number+" ");
+			n--;
+		}
+	}
+
+}
